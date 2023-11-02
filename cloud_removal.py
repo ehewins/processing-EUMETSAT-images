dd9f5e4cb061211acea2d3717d654032f5eb7a24
Ellis Hewins <ellishewins@gmail.com>
2023-11-02 22:40:55 +0000

    Applied time averaging process to coloured images

--- cloud_removal.py
+++ cloud_removal.py
@@ -7,11 +7,11 @@ import file_selection
 START, END = '01/01/2019', '07/01/2019'
 IR16, VIS8, VIS6 = file_selection.load_in_range('all', START, END)
 # VIS6 = file_selection.load_in_range('VIS6', START, END).astype(np.uint32)
-# colour = np.stack((IR16, VIS8, VIS6), axis=-1).astype(np.uint32)
+colour = np.stack((IR16, VIS8, VIS6), axis=-1).astype(np.uint32)
 
 p = 4  # power the reciprocal is raised to during weighting.
-window_size = 7  # number of images averaged over during rolling average
-array = VIS6.astype(np.uint32)  # Change VIS6 to VIS8 or IR16 to test them out
+window_size = 9  # number of images averaged over during rolling average
+array = colour
 array_size = array.shape[0]
 rolling_average = np.empty((0, *array.shape[1:]))
 for i in range(array_size):
@@ -24,8 +24,8 @@ for i in range(array_size):
 
 # Display before/after of the image from the middle of the timespan
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
-ax1.imshow(array[7], cmap='gray')
-ax2.imshow(rolling_average[7], cmap='gray')
+ax1.imshow(array[7].astype(np.uint8))
+ax2.imshow(rolling_average[7].astype(np.uint8))
 for ax in (ax1, ax2):
     ax.set_axis_off()
 ax1.set_title("Original image")
