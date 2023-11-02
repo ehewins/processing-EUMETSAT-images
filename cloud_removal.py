3d693ab06870e1be3b03cb4dfe84959d2b7a71b5
Ellis Hewins <ellishewins@gmail.com>
2023-11-02 20:42:30 +0000

    Upgraded cloud_removal.py to use entire monochrome images.

--- cloud_removal.py
+++ cloud_removal.py
@@ -4,41 +4,31 @@ import matplotlib.pyplot as plt
 
 import file_selection
 
-START, END = '01/01/2019', '01/03/2019'
-# IR16, VIS8, VIS6 = file_selection.load_in_range('all', START, END)
-VIS6 = file_selection.load_in_range('VIS6', START, END).astype(np.uint32)
-# # colour = np.stack((IR16, VIS8, VIS6), axis=-1)
+START, END = '01/01/2019', '07/01/2019'
+IR16, VIS8, VIS6 = file_selection.load_in_range('all', START, END)
+# VIS6 = file_selection.load_in_range('VIS6', START, END).astype(np.uint32)
+# colour = np.stack((IR16, VIS8, VIS6), axis=-1).astype(np.uint32)
 
-# IR16_central_pixel = IR16[:, 1856, 1856]
-# VIS8_central_pixel = VIS8[:, 1856, 1856]
-VIS6_central_pixel = VIS6[:, 1856, 1856]
+p = 4  # power the reciprocal is raised to during weighting.
+window_size = 7  # number of images averaged over during rolling average
+array = VIS6.astype(np.uint32)  # Change VIS6 to VIS8 or IR16 to test them out
+array_size = array.shape[0]
+rolling_average = np.empty((0, *array.shape[1:]))
+for i in range(array_size):
+    window_start = max(i - (window_size - 1) // 2, 0)
+    window_end = min(i + (window_size - 1) // 2, array_size)
+    in_window = array[window_start: window_end + 1]
+    weighted_average = np.sum(in_window/in_window**p, axis=0) / \
+        np.sum(1/in_window**p, axis=0)
+    rolling_average = np.append(rolling_average, [weighted_average], axis=0)
 
-# plt.plot(IR16_central_pixel, label="IR16")
-# plt.plot(VIS8_central_pixel, label="VIS8")
-plt.figure(figsize=(12, 9))
-plt.plot(VIS6_central_pixel, label="VIS6 data")
+# Display before/after of the image from the middle of the timespan
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+ax1.imshow(array[7], cmap='gray')
+ax2.imshow(rolling_average[7], cmap='gray')
+for ax in (ax1, ax2):
+    ax.set_axis_off()
+ax1.set_title("Original image")
+ax2.set_title("Rolling time average with window size {:d}".format(window_size))
 
-p = 3  # power the reciprocal is raised to during weighting.
-for window_size in (3, 5, 7, 9, 11):
-    array = VIS6_central_pixel
-    array_size = array.size
-    rolling_average = []
-    for i in range(array_size):
-        window_start = i - (window_size - 1) // 2
-        window_end = i + (window_size - 1) // 2
-        if window_start < 0:
-            window_start = 0
-        if window_end > array_size:
-            window_end = array_size
-        in_window = array[window_start: window_end + 1]
-        weighted_average = np.sum(in_window/in_window**p) / np.sum(1/in_window**p)
-        rolling_average.append(weighted_average)
-
-    plt.plot(rolling_average,
-             label="Window size = {:d} images".format(window_size))
-
-plt.title("Rolling weighted average (p={:d}) over pixel data, with varying window sizes".format(p))
-plt.xlabel("Image number")
-plt.ylabel("Pixel value")
-plt.legend()
 plt.show()
