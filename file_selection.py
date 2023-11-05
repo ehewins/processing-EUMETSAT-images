affd0d5b1ec4349e2c16088b9e429a2fd06e8484
Ellis Hewins <ellishewins@gmail.com>
2023-11-05 21:22:45 +0000

    Moved the filename & date sorting code to its own function, and added a function to generate and load colour images between a start and end datetime.datetime object.

--- file_selection.py
+++ file_selection.py
@@ -4,6 +4,10 @@ This program loads which files for the data analysis process to use.
 
 import os
 from datetime import datetime
+# These two lines fix a problem with some images not loading properly
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,7 +16,35 @@ MIN_DATE = datetime(2019, 1, 1, 0, 0, 0)
 MAX_DATE = datetime(2020, 1, 1, 23, 59, 59)
 
 
-def load_in_range(wavelength='all', start='01/01/2019', end='01/01/2020'):
+def sorted_filenames_and_dates():
+    """
+    This function reads the names of the files in the IMAGE_DIR directory, and
+    returns an alphabetically and chronologically ordered list of these file
+    names, along with a list of datetime objects defining when each image was
+    taken.
+    """
+    # Get alphabetically + chronologically sorted list of files
+    file_list = os.listdir(IMAGE_DIR)
+    wavelength_list = []
+    timestamp_list = []
+    for file_name in file_list:
+        file_wavelength = file_name[0:4]
+        file_timestamp = datetime(int(file_name[29:33]),  # year
+                                  int(file_name[33:35]),  # month
+                                  int(file_name[35:37]),  # day
+                                  int(file_name[37:39]),  # hour
+                                  int(file_name[39:41]),  # minute
+                                  int(file_name[41:43]))  # second
+        wavelength_list.append(file_wavelength)
+        timestamp_list.append(file_timestamp)
+    sort_indices = np.lexsort((timestamp_list, wavelength_list))
+    file_list = np.array(file_list)[sort_indices]
+    timestamp_list = np.array(timestamp_list)[sort_indices]
+
+    return file_list, timestamp_list
+
+
+def load_dates(wavelength='all', start='01/01/2019', end='01/01/2020'):
     """
     This function is used to load files from a specific range of dates, in a
     specific wavelength. Returns an array of images. If wavelength == 'all',
@@ -38,23 +70,7 @@ def load_in_range(wavelength='all', start='01/01/2019', end='01/01/2020'):
     if wavelength not in ['all', 'IR16', 'VIS6', 'VIS8']:
         raise ValueError("Invalid wavelength parameter.")
 
-    # Get alphabetically + chronologically sorted list of files
-    file_list = os.listdir(IMAGE_DIR)
-    wavelength_list = []
-    timestamp_list = []
-    for file_name in file_list:
-        file_wavelength = file_name[0:4]
-        file_timestamp = datetime(int(file_name[29:33]),  # year
-                                  int(file_name[33:35]),  # month
-                                  int(file_name[35:37]),  # day
-                                  int(file_name[37:39]),  # hour
-                                  int(file_name[39:41]),  # minute
-                                  int(file_name[41:43]))  # second
-        wavelength_list.append(file_wavelength)
-        timestamp_list.append(file_timestamp)
-    sort_indices = np.lexsort((timestamp_list, wavelength_list))
-    file_list = np.array(file_list)[sort_indices]
-    timestamp_list = np.array(timestamp_list)[sort_indices]
+    file_list, timestamp_list = sorted_filenames_and_dates()
 
     # Take slice of list for specific wavelengths
     # If wavelength == 'all', then no slice will be taken.
@@ -85,3 +101,38 @@ def load_in_range(wavelength='all', start='01/01/2019', end='01/01/2020'):
         return IR16, VIS8, VIS6
 
     return np.asarray(images)
+
+
+def load_datetimes_three_colour(start, end):
+    """
+    This function is used to create an array of three-colour images spanning a
+    time interval defined using datetime objects.
+    Files start at 'start', and run up to but not including 'end'.
+    """
+    # Input validation
+    if not isinstance(start, datetime):
+        raise TypeError("Parameter 'start' must be a datetime object.")
+    if not isinstance(end, datetime):
+        raise ValueError("Parameter 'end' must be a datetime object.")
+    if start > end:
+        raise ValueError("Start date after end date")
+    if start < MIN_DATE:
+        raise ValueError("Start date before 01/01/2019")
+    if end > MAX_DATE:
+        raise ValueError("Start date after 01/01/2020")
+
+    # Get all filenames and dates and store ones from the desired time interval
+    file_list, timestamp_list = sorted_filenames_and_dates()
+    file_list = file_list[(timestamp_list >= start) & (timestamp_list < end)]
+    IR16_files, VIS6_files, VIS8_files = np.array_split(file_list, 3)
+
+    colour_images = []
+    for n in range(len(file_list) // 3):
+        # Load three monochrome image files into one colour image
+        image = np.stack((
+            plt.imread(os.path.join(IMAGE_DIR, IR16_files[n])),
+            plt.imread(os.path.join(IMAGE_DIR, VIS8_files[n])),
+            plt.imread(os.path.join(IMAGE_DIR, VIS6_files[n]))), axis=-1)
+        colour_images.append(image)
+
+    return np.asarray(colour_images).astype(np.uint8)
