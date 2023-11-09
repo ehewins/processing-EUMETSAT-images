e83e92a668c74ff87517e497b21dacf1a91ab987
Ellis Hewins <ellishewins@gmail.com>
2023-11-09 22:49:30 +0000

    Analysis of vegetation area and mean NDVI of vegetation in three regions.

--- NDVI.py
+++ NDVI.py
@@ -1,49 +1,71 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage.measure import label, regionprops
-from skimage.morphology import binary_closing
 
 IMAGE_DIR = 'cloud_free_images'
 # There are 52 images - one for each week of the year
-cloud_free_files = ["week_{:d}.jpg".format(w) for w in range(5)]
-image_shape = np.shape(plt.imread(os.path.join(IMAGE_DIR,
-                                               cloud_free_files[0])))
-image_count = len(cloud_free_files)
+weeks = range(52)
+cloud_free_files = ["week_{:d}.jpg".format(w) for w in weeks]
+
+# Define y_min, y_max, x_min, x_max for regions of analysis
+regions = {
+    "Africa": [1400, 3000, 1250, 3350],
+    "South America": [1400, 3200, 0, 700],
+    "Europe & Middle East": [0, 1000, 1500, 3250]
+}
+# Define dictionaries to store weekly values of the regions' properties
+vegetation_area = {
+    "Africa": [],
+    "South America": [],
+    "Europe & Middle East": []
+}
+mean_NDVI = {
+    "Africa": [],
+    "South America": [],
+    "Europe & Middle East": []
+}
 
 # Compute the Normalised Difference Vegetation Index for all images
-detected_vegetation = np.empty((0, image_shape[0], image_shape[1]))
 for file_name in cloud_free_files:
     image = plt.imread(os.path.join(IMAGE_DIR, file_name))
-    NVDI = (image[:, :, 1] - image[:, :, 2]) / \
+    NDVI = (image[:, :, 1] - image[:, :, 2]) / \
         (image[:, :, 1] + image[:, :, 2])
-    vegetation = (NVDI >= 0.25) & (NVDI <= 0.8)
-    detected_vegetation = np.append(detected_vegetation, [vegetation], axis=0)
+    vegetation = (NDVI >= 0.3) & (NDVI <= 0.8)
+    for region_name, slice_def in regions.items():
+        region_greenery = vegetation[slice_def[0]: slice_def[1],
+                                     slice_def[2]: slice_def[3]]
+        region_NDVI = NDVI[slice_def[0]: slice_def[1],
+                           slice_def[2]: slice_def[3]]
+        greenery_NDVI = region_greenery * region_NDVI
+        green_area = np.sum(region_greenery)
+        mean_greenery_NDVI = np.nanmean(greenery_NDVI[greenery_NDVI != 0])
+        vegetation_area[region_name].append(green_area)
+        mean_NDVI[region_name].append(mean_greenery_NDVI)
+    # Uncomment the following lines to display regions satisfying the threshold
+    # (Consider changing the 'weeks' variable to a smaller range for speed)
     # display_image = np.copy(image)
     # display_image[vegetation] = 255 * \
-    #     np.stack((np.zeros_like(NVDI),
-    #               np.ones_like(NVDI),
-    #               np.zeros_like(NVDI)), axis=-1)[vegetation]
+    #     np.stack((np.zeros_like(NDVI),
+    #               np.ones_like(NDVI),
+    #               np.zeros_like(NDVI)), axis=-1)[vegetation]
     # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
     # ax1.imshow(image.astype(np.uint8))
     # plt.imshow(display_image.astype(np.uint8))
     # ax1.set_title("Original image")
     # ax2.set_title("Highlighted vegetation")
 
-# Label each connected region to analyse how they change over time
-for veg_map in detected_vegetation:
-    binary_closing(veg_map, footprint=np.ones((15, 15)), out=veg_map)
-    labelled_blobs = label(veg_map)
-    props = regionprops(labelled_blobs)
-    blob_areas = [x.area for x in props]
-    # Find largest 10 areas, in descending order.
-    largest_blobs_indices = np.argsort(blob_areas)[:-11:-1]
-    largest_blobs_centroids = [props[i].centroid for i in largest_blobs_indices]
-    plt.figure()
-    plt.imshow(veg_map, cmap='gray')
-    for n, centroid in enumerate(largest_blobs_centroids):
-        plt.plot([centroid[1]], [centroid[0]], 'o', label='Number {:d}'.format(n))
-    plt.legend()
+# Rescale the area arrays so they show relative change from the mean value
+for region in vegetation_area:
+    vegetation_area[region] /= np.mean(vegetation_area[region])
 
+fig, ((ax1), (ax2)) = plt.subplots(2, 1, figsize=(14, 6))
+ax1.set_xlabel("Week number")
+ax2.set_xlabel("Week number")
+ax1.set_ylabel("Relative area of vegetation")
+ax2.set_ylabel("Mean NDVI of vegetation")
+for region in regions:
+    ax1.plot(vegetation_area[region], label=region)
+    ax2.plot(mean_NDVI[region], label=region)
+plt.legend()
 
 plt.show()
