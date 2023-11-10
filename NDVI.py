ed9c22579525c8b0371fa0d571ed154a44a31674
Ellis Hewins <ellishewins@gmail.com>
2023-11-10 08:31:11 +0000

    Added code (commented out) to highlight regions of interest.

--- NDVI.py
+++ NDVI.py
@@ -2,6 +2,9 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+from Load_clouds import clouds
+from general_statistics import calculate_yearly_mean
+
 IMAGE_DIR = 'cloud_free_images'
 # There are 52 images - one for each week of the year
 weeks = range(52)
@@ -9,10 +12,24 @@ cloud_free_files = ["week_{:d}.jpg".format(w) for w in weeks]
 
 # Define y_min, y_max, x_min, x_max for regions of analysis
 regions = {
-    "Africa": [1400, 3000, 1250, 3350],
+    "Africa": [1350, 3000, 1250, 3350],
     "South America": [1400, 3200, 0, 700],
-    "Europe & Middle East": [0, 1000, 1500, 3250]
+    "Europe & Middle East": [0, 900, 1500, 3250]
 }
+
+# # Display these regions for use in a figure
+# mean_image = calculate_yearly_mean()
+# plt.imshow(mean_image.astype(np.uint8))
+# for r in regions:
+#     # top left, top right, bottom left and bottom right corner coords
+#     tl = [regions[r][2], regions[r][0]]
+#     tr = [regions[r][3], regions[r][0]]
+#     bl = [regions[r][2], regions[r][1]]
+#     br = [regions[r][3], regions[r][1]]
+#     plt.plot([tl[0], tr[0], br[0], bl[0], tl[0]],
+#              [tl[1], tr[1], br[1], bl[1], tl[1]], linewidth=2)
+# plt.show()
+
 # Define dictionaries to store weekly values of the regions' properties
 vegetation_area = {
     "Africa": [],
@@ -54,18 +71,37 @@ for file_name in cloud_free_files:
     # ax1.set_title("Original image")
     # ax2.set_title("Highlighted vegetation")
 
-# Rescale the area arrays so they show relative change from the mean value
-for region in vegetation_area:
-    vegetation_area[region] /= np.mean(vegetation_area[region])
+# Perform similar analysis on clouds in the regions
+cloud_coverage = {
+    "Africa": [],
+    "South America": [],
+    "Europe & Middle East": []
+}
+# Involves performing the entire thresholding process, so this takes a while
+for week in weeks:
+    # Generate weekly cloud density maps
+    weekly_clouds, _ = clouds(week)
+    sum_cloud_coverage = np.sum(weekly_clouds, axis=0)
+    for region, region_def in regions.items():
+        region_coverage = sum_cloud_coverage[region_def[0]: region_def[1],
+                                             region_def[2]: region_def[3]]
+        cloud_coverage[region].append(np.sum(region_coverage))
 
-fig, ((ax1), (ax2)) = plt.subplots(2, 1, figsize=(14, 6))
-ax1.set_xlabel("Week number")
-ax2.set_xlabel("Week number")
-ax1.set_ylabel("Relative area of vegetation")
-ax2.set_ylabel("Mean NDVI of vegetation")
+# Rescale the area arrays so they show relative change from the mean value
 for region in regions:
-    ax1.plot(vegetation_area[region], label=region)
-    ax2.plot(mean_NDVI[region], label=region)
-plt.legend()
+    vegetation_area[region] /= np.mean(vegetation_area[region])
+    cloud_coverage[region] /= np.mean(cloud_coverage[region])
+
+fig, ((ax1), (ax2), (ax3)) = plt.subplots(3, 1, figsize=(10, 14))
+for region in regions:
+    ax1.plot(mean_NDVI[region], label=region)
+    ax2.plot(vegetation_area[region], label=region)
+    ax3.plot(cloud_coverage[region], label=region)
+for ax in (ax1, ax2, ax3):
+    ax.legend(loc='upper right')
+    ax.set_xlabel("Week number")
+ax1.set_ylabel("Mean NDVI of vegetation")
+ax2.set_ylabel("Relative area of vegetation")
+ax3.set_ylabel("Relative cloud coverage")
 
 plt.show()
